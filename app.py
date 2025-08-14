7c9daf0d6b805d805afc9cf2377179e08b220806
Akhmad Khudri <khudri@binadarma.ac.id>
2025-08-14 15:54:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.set_page_config(
 st.title("📊 Exploratory Data Analysis Dashboard")
 st.markdown("""
 <span style='font-size:16px; color:white'>
-💡 <b>STC Insight</b> adalah dasbor visual interaktif untuk menganalisis data pemesanan dan biaya dalam ekosistem <a href="https://smartourism.elpeef.com/">SmartTourismChain (STC)</a>
+💡 <b>STC Insight</b> adalah dasbor visual interaktif untuk menganalisis data pemesanan dan biaya dalam ekosistem <a href="https://smartourism.elpeef.com/" target="_blank">SmartTourismChain (STC)</a>
 
 Dashboard ini dirancang untuk membantu pelaku industri, peneliti, dan pengembang dalam mengeksplorasi data transaksi wisata secara cepat dan intuitif, mulai dari tren durasi inap, total biaya, hingga metode pembayaran dan status transaksi.
 Fitur:
